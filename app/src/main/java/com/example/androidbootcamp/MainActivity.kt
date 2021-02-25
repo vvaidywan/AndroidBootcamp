c4c5506d7596e847497516a464a9c36530ab0a06
Varsha <varsha.vaidywan@tothenew.com>
2021-02-25 14:56:10 +0530

    login screen added

--- app/src/main/java/com/example/androidbootcamp/MainActivity.kt
+++ app/src/main/java/com/example/androidbootcamp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.androidbootcamp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    //var btn1 : Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn1 = findViewById<Button>(R.id.btn1)
+
+        btn1.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this@MainActivity, HomePage::class.java)
+            startActivity(intent)
+        })
+
     }
 }

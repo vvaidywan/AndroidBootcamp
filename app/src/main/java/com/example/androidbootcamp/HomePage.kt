a92ddd06e6553f212510691315222f7729495210
Varsha <varsha.vaidywan@tothenew.com>
2021-02-26 19:24:51 +0530

    Questions done

--- app/src/main/java/com/example/androidbootcamp/HomePage.kt
+++ app/src/main/java/com/example/androidbootcamp/HomePage.kt
@@ -1,11 +1,91 @@
 package com.example.androidbootcamp
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
 
 class HomePage : AppCompatActivity() {
+
+    private val CAMERA_REQUEST_CODE = 1804
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+        var tvName = findViewById<TextView>(R.id.tvName)
+        var tvEmail = findViewById<TextView>(R.id.tvEmail)
+        var tvPhone = findViewById<TextView>(R.id.tvPhone)
+        var tvPass = findViewById<TextView>(R.id.tvPass)
+
+        var strName  = intent.getStringExtra("name")
+        var strEmail = intent.getStringExtra("email")
+        var strPhone = intent.getStringExtra("phone")
+        var strPass = intent.getStringExtra("password")
+
+        tvName.setText(strName)
+        tvEmail.setText(strEmail)
+        tvPhone.setText(strPhone)
+        tvPass.setText(strPass)
+
+        var btnUrl = findViewById<Button>(R.id.btnUrl)
+        var btnPermission = findViewById<Button>(R.id.btnPermission)
+        var etUrl = findViewById<EditText>(R.id.etUrl)
+
+
+
+        btnUrl.setOnClickListener(View.OnClickListener {
+            if (etUrl.text.toString().isNullOrBlank()) {
+                etUrl.error = "Enter url"
+            } else {
+                var url: String = etUrl.text.toString()
+                val i = Intent(Intent.ACTION_VIEW)
+                i.setData(Uri.parse(url))
+                startActivity(i)
+            }
+        })
+
+        btnPermission.setOnClickListener(View.OnClickListener {
+            val value = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+            when (value) {
+                PackageManager.PERMISSION_GRANTED -> {
+                    Log.e("USER_PERMISSION", "PERMISSION_GRANTED")
+                    Toast.makeText(this, "Permission already granted", Toast.LENGTH_SHORT).show()
+                }
+                PackageManager.PERMISSION_DENIED -> {
+                    ActivityCompat.requestPermissions(this,
+                        arrayOf(android.Manifest.permission.CAMERA),
+                        CAMERA_REQUEST_CODE)
+                }
+            }
+        })
+
+
+
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || (grantResults[0] != PackageManager.PERMISSION_GRANTED)) {
+                    Log.i("TAG", "Permission has been denied by user")
+                    Toast.makeText(this, "Permission denied by user", Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.i("TAG", "Permission has been granted by user")
+                    Toast.makeText(this, "Permission granted by user", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/androidbootcamp/MainActivity.kt
+++ app/src/main/java/com/example/androidbootcamp/MainActivity.kt
@@ -5,20 +5,50 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+
+/* Question : Create one signup activity having “user name”, “email-id”, “phone number”, “password”
+with all fields mandatory.
+Explicit Intent : On clicking on signup button user should able to see all the filled details on
+the next screen.
+Implicit Intent : On the second Activity there should be a input box and two button along with the
+above details. the user should be able to enter a website url and on clicking one of the button an
+implicit intent should be created to open a web browser with the url entered by the user.
+Permission : On clicking on the second button check whether the app has the camera permission
+granted or not if permission is granted display permission is granted otherwise ask user for
+    permission and display whether the permission is granted or not.*/
 
 class MainActivity : AppCompatActivity() {
-
-    //var btn1 : Button? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var btn1 = findViewById<Button>(R.id.btn1)
+        var name = findViewById<EditText>(R.id.name)
+        var email = findViewById<EditText>(R.id.email)
+        var phone = findViewById<EditText>(R.id.phone)
+        var password = findViewById<EditText>(R.id.password)
+        var signup = findViewById<Button>(R.id.signup)
 
-        btn1.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this@MainActivity, HomePage::class.java)
-            startActivity(intent)
+        signup.setOnClickListener(View.OnClickListener {
+            if(name.text.toString().isNullOrBlank()){
+                name.error = "Please enter name"
+            }
+            else if(email.text.toString().isNullOrBlank()){
+                email.error = "Please enter email"
+            }
+            else if(phone.text.toString().isNullOrBlank()){
+                phone.error = "Please enter phone number"
+            }
+            else if(password.text.toString().isNullOrBlank()){
+                password.error = "Please enter name"
+            }else {
+                val intent = Intent(this@MainActivity, HomePage::class.java)
+                intent.putExtra("name", name.text.toString())
+                intent.putExtra("email", email.text.toString())
+                intent.putExtra("phone", phone.text.toString())
+                intent.putExtra("password", password.text.toString())
+                startActivity(intent)
+            }
         })
 
     }
